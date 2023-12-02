ef927e1b38cda6bb1771dd81c7ed8dbd6190da00
USJID <usjidn@gmail.com>
2023-12-02 22:08:38 +0500

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import LandingPage from "./Pages/LandingPage"
 export default function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div>
+        <LandingPage />
+      </div>
     )
   }
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Blog from "../Components/Blog";
+import Footer from "../Components/Footer";
+export default function LandingPage() {
+    return (
+      <div>
+        <Header />
+        <Blog />
+        <Footer />
+      </div>
+    )
+  }
