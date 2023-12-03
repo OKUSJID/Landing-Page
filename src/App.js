6917a49a1dd5046924d8c148a423e840bf8ce290
USJID <usjidn@gmail.com>
2023-12-03 14:37:14 +0500

    Upadated Footer and fixed Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import LandingPage from "./Pages/LandingPage"
-export default function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../src/Components/Header'; // Import your Header component
+import Footer from '../src/Components/Footer'; // Import your Footer component
+import Blog from '../src/Components/Blog'; // Import your page components
+import Pricing from '../src/Components/Pricing'; // Another page component
+// Import other pages as needed
+
+function App() {
     return (
-      <div>
-        <LandingPage />
-      </div>
-    )
-  }
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Blog />} />
+                <Route path="/pricing" element={<Pricing />} />
+                {/* Define other routes as needed */}
+            </Routes>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
